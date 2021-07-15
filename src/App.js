67719dd628a7bbca5e68f899c89f9be161a18724
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-07-16 02:52:55 +0900

    gnb-3:  html 작성하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
+import NavBar from './components/NavBar';
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>header</header>
+			<NavBar />
 			<main>main</main>
 		</div>
 	);
