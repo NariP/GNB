e9ead9f1f8d1a2b07c3dbff785f9292e8497077a
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-07-15 17:19:11 +0900

    gnb-1: 프로젝트 세팅

---
+++ src/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	endOfLine: 'lf',
+	trailingComma: 'all',
+	bracketSpacing: true,
+	jsxBracketSameLine: false, // 여러줄의 JSX 요소가 있을때, > 를 마지막 줄의 끝부분에서 닫을 것인지
+	arrowParens: 'always', // (x) => x : always | x => x : avoid
+};
---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>header</header>
+			<main>main</main>
+		</div>
+	);
+}
+
+export default App;
