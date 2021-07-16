75d8f9debe6b15ea7dad10599c347e7b7f5879cc
DESKTOP-IS1973C\Admin <tto9208@naver.com>
2021-07-17 01:36:14 +0900

    gnb-4: html 작성하기 & 공통 스타일 적용

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -43,6 +43,235 @@ function NavBar(props) {
 							</a>
 						</li>
 					</ul>
+					{/*드롭다운 메뉴*/}
+					<div>
+						<div className='container'>
+							<div className='row'>
+								<div>
+									<a href='/'>
+										<h2>
+											영업<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h3>기업영업</h3>
+									</a>
+									<a href='/'>
+										<h3>외부영업</h3>
+									</a>
+									<a href='/'>
+										<h3>영업 관리자</h3>
+									</a>
+									<a href='/'>
+										<h3>기술영업</h3>
+									</a>
+									<a href='/'>
+										<h3>주요고객사 담당자</h3>
+									</a>
+									<a href='/'>
+										<h3>솔루션 컨설턴트</h3>
+									</a>
+									<a href='/'>
+										<h3>해외영업</h3>
+									</a>
+									<a href='/'>
+										<h3>
+											더보기<i>icon(_arrow_right_>)</i>
+										</h3>
+									</a>
+								</div>
+								<div>
+									<a href='/'>
+										<h2>
+											미디어<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h3>콘텐츠 크리에이터</h3>
+									</a>
+									<a href='/'>
+										<h3>PD</h3>
+									</a>
+									<a href='/'>
+										<h3>영상 편집가</h3>
+									</a>
+									<a href='/'>
+										<h3>에디터</h3>
+									</a>
+									<a href='/'>
+										<h3>비디오 제작</h3>
+									</a>
+									<a href='/'>
+										<h3>작가</h3>
+									</a>
+									<a href='/'>
+										<h3>출판 기획자</h3>
+									</a>
+									<a href='/'>
+										<h3>
+											더보기<i>icon(_arrow_right_>)</i>
+										</h3>
+									</a>
+								</div>
+								<div>
+									<a href='/'>
+										<h2>
+											인사<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h3>인사담당</h3>
+									</a>
+									<a href='/'>
+										<h3>리쿠르터</h3>
+									</a>
+									<a href='/'>
+										<h3>조직문화</h3>
+									</a>
+									<a href='/'>
+										<h3>평가·보상</h3>
+									</a>
+									<a href='/'>
+										<h3>헤드헌터</h3>
+									</a>
+									<a href='/'>
+										<h3>HRBP</h3>
+									</a>
+									<a href='/'>
+										<h3>HRD</h3>
+									</a>
+									<a href='/'>
+										<h3>
+											더보기<i>icon(_arrow_right_>)</i>
+										</h3>
+									</a>
+								</div>
+								<div>
+									<a href='/'>
+										<h2>
+											게임 제작
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h3>게임 기획자</h3>
+									</a>
+									<a href='/'>
+										<h3>게임 그래픽 디자이너</h3>
+									</a>
+									<a href='/'>
+										<h3>모바일 게임 개발자</h3>
+									</a>
+									<a href='/'>
+										<h3>게임 아티스트</h3>
+									</a>
+									<a href='/'>
+										<h3>게임 클라이언트 개발자</h3>
+									</a>
+									<a href='/'>
+										<h3>게임 서버 개발자</h3>
+									</a>
+									<a href='/'>
+										<h3>유니티 개발자</h3>
+									</a>
+									<a href='/'>
+										<h3>
+											더보기<i>icon(_arrow_right_>)</i>
+										</h3>
+									</a>
+								</div>
+								<div>
+									<a href='/'>
+										<h2>
+											엔지니어링·설계
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h3>전자 엔지니어</h3>
+									</a>
+									<a href='/'>
+										<h3>기계 엔지니어</h3>
+									</a>
+									<a href='/'>
+										<h3>전기 엔지니어</h3>
+									</a>
+									<a href='/'>
+										<h3>전기기계 공학자</h3>
+									</a>
+									<a href='/'>
+										<h3>로봇 자동화</h3>
+									</a>
+									<a href='/'>
+										<h3>QA 엔지니어</h3>
+									</a>
+									<a href='/'>
+										<h3>제품 엔지니어</h3>
+									</a>
+									<a href='/'>
+										<h3>
+											더보기<i>icon(_arrow_right_>)</i>
+										</h3>
+									</a>
+								</div>
+								<div>
+									<a href='/'>
+										<h2>
+											금융<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											제조·생산
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											물류·무역
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											의료·제약·바이오
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											교육
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											법률·법집행기관
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											식·음료
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											건설·시설
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+									<a href='/'>
+										<h2>
+											공공·복지
+											<i>icon(_arrow_right_>)</i>
+										</h2>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
 					<aside>
 						<ul>
 							<li>
